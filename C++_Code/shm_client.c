70bf97aa16f620b700bf3d893c7d2deb913d0549
xuelangZF <xuezaigds@gmail.com>
2016-06-09 20:42:28 +0800

    Add instance about shared memory

---
+++ C++_Code/shm_client.c
@@ -0,0 +1,46 @@
+/*
+* @Author: xuelangZF
+* @Date:   2016-06-09 20:18:58
+* @Last Modified time: 2016-06-09 20:38:31
+* Refer to: https://www.cs.cf.ac.uk/Dave/C/node27.html
+*/
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SHMSZ     27
+
+int main()
+{
+    int shmid;
+    key_t key;
+    char *shm, *s;
+
+    // We need to get the segment named "5678", created by the server.
+    key = 5678;
+
+    // Locate the segment.
+    if ((shmid = shmget(key, SHMSZ, 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+
+    // Now we attach the segment to our data space.
+    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    // Now read what the server put in the memory.
+    for (s = shm; *s != 0; s++)
+        putchar(*s);
+    putchar('\n');
+
+    /*
+     * Finally, change the first character of the segment to '*',
+     * indicating we have read the segment.
+     */
+    *shm = '*';
+
+    exit(0);
+}
---
+++ C++_Code/shm_server.c
@@ -0,0 +1,50 @@
+/*
+* @Author: xuelangZF
+* @Date:   2016-06-09 20:15:34
+* @Last Modified time: 2016-06-09 20:38:04
+* According to: https://www.cs.cf.ac.uk/Dave/C/node27.html
+*/
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SHMSZ     27
+
+int main()
+{
+    char c;
+    int shmid;
+    key_t key;
+    char *shm, *s;
+
+    // We'll name our shared memory segment "5678".
+    key = 5678;
+
+    // Create the segment.
+    if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+
+    // Now we attach the segment to our data space.
+    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    // Now put some things into the memory for the other process to read.
+    s = shm;
+    for (c = 'a'; c <= 'z'; c++)
+        *s++ = c;
+    *s = 0;
+
+    /*
+     * Finally, we wait until the other process changes the first character of our memory
+     * to '*', indicating that it has read what we put there.
+     */
+    while (*shm != '*')
+        sleep(1);
+
+    exit(0);
+}
